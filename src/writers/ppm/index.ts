84bc647a57ba953a6f3956fae33b96bea3a5f9c4
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-03-10 18:01:09 +0200

    add writer for ppm

---
+++ src/writers/ppm/index.ts
@@ -0,0 +1,19 @@
+import { Image } from "src/types/image";
+import { IImageWriter } from "src/types/writer";
+
+export const ppmWriter: IImageWriter = {
+  write(image: Image): Buffer {
+    const width = image[0].length;
+    const height = image.length;
+    const imageStr = image
+      .map(row => row.map(val => `${val.red} ${val.green} ${val.blue}`).join('\t'))
+      .join('\n');
+    const maxColor = 255;
+
+    return Buffer.from(
+      'P3\n' +
+      `${width} ${height} ${maxColor}\n` +
+      `${imageStr}`
+      , 'ascii')
+  }
+}
