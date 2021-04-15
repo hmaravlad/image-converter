c980b9ba07c707c9761ea24bf05c7e425980dd97
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-04-15 16:16:45 +0300

    modify main file for rendering

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { readFile, writeFile } from 'fs';
 import { promisify } from 'util';
 import { parse } from 'ts-command-line-args';
-import { readerFactory } from './readers/readerFactory';
 import {
   IRenderImageArgsRaw,
   ConvertRenderImageArgsConfig
@@ -9,15 +8,38 @@ import {
 import { processArgs } from './util/processArgs';
 import { writerFactory } from './writers/writerFactory';
 import { generateFileName } from './util/generateFileName';
+import RayTraceRender from './renders/rayTraceRender';
+import ObjParser from './util/objParser';
+import { Options } from './models/options';
+import Vector3D from './models/vector3D';
+import { Converter } from './util/converter';
+import Light from './models/light';
 
 (async () => {
   const argsRaw = parse<IRenderImageArgsRaw>(ConvertRenderImageArgsConfig);
   const args = processArgs(argsRaw);
 
-  const originalBuffer = await promisify(readFile)(args.source);
-  const imageReader = readerFactory.getReader(args.source);
-  const image = imageReader.read(originalBuffer);
+  const objString = (await promisify(readFile)(args.source)).toString();
+  const parser = new ObjParser(objString);
+  const data = parser.parse();
 
+  const width = 100; const height = 100;
+
+  const renderer = new RayTraceRender(
+    [new Light(new Vector3D(0, 2, 0), 3)],
+    new Options(
+      height,
+      width,
+      0.2,
+      150,
+      new Vector3D(255, 255, 255),
+      new Vector3D(108, 34, 25),
+      new Vector3D(0, 2, 0),
+    )
+  );
+
+  const renderResult = renderer.render(data);
+  const image = (new Converter()).convert(renderResult);
   const imageWriter = writerFactory.getWriter(args.imageFormat);
   const resultBuffer = imageWriter.write(image);
   const fileName = generateFileName(args.source, args.output, args.imageFormat);
