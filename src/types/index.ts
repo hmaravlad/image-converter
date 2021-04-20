b74f35d0e081dd2873dd983f48bc1cdb0db17a54
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-04-20 13:38:33 +0300

    add type identifiers for di

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export const TYPES = {
+  ITreeFactory: Symbol.for("ITreeFactory"),
+  IBoxSplitter: Symbol.for("IBoxSplitter"),
+  IRendererFactory: Symbol.for("IRendererFactory"),
+  IReaderFactory: Symbol.for("IReaderFactory"),
+  IOptionsProvider: Symbol.for("IOptionsProvider"),
+  ILightProvider: Symbol.for("ILightProvider"),
+}
